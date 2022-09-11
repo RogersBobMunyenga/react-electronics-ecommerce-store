2d82c9e895aeb229e8958dfbdebf7827159880a0
RogersBobMunyenga <b.munyenga@gmail.com>
2022-09-11 21:53:59 +0300

    initial

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Head from "./common/Head";
+import Navbar from "./common/Navbar";
+import Search from "./common/Search";
+
+function App() {
+  return <>
+    
+  <Head/>
+  <Search/>
+    <Navbar />
+     </>
+  
+  
+}
+
+export default App;
---
+++ src/common/Head.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+// import PhoneIcon from '@mui/icons-material/Phone';
+const Head = () => {
+  return (
+      <>
+          <div className="head">
+              <div className="container d-flex">
+                {/* <PhoneIcon/> */}
+              </div>
+          </div>
+      </>
+  )
+}
+
+export default Head
